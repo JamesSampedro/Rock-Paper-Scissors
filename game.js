216dd2e059320acb65d74afbc2b85c1eca5e552a
James Paul Sampedro <jamez.8833@gmail.com>
2024-06-05 15:49:39 +0800

    reordered the function

--- game.js
+++ game.js
@@ -5,25 +5,20 @@ function getComputerChoice () {
     return randomChoice;
 }
 
-getComputerChoice();
-
-
 function getHumanChoice () {
     let humanChoice = prompt("rock, paper or scissors?\nChoice: ").toLowerCase();
     if (humanChoice === "rock") {
         console.log("You played rock.");
-
     } else if (humanChoice === "paper") {
         console.log("You played paper.");
-
     } else if (humanChoice === "scisssors") {
         console.log("You played scissors.");
-
     }else {
         console.log("Error. You didn't pick between the three choices.");
     }
     return humanChoice;
 }
 getHumanChoice();
+getComputerChoice();
 
 console.log("test");
