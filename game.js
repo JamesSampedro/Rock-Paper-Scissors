8be1eb122d22644137dd7bcf7069b91b333fb1f5
James Paul Sampedro <jamez.8833@gmail.com>
2024-06-05 15:48:24 +0800

    added humanchoice prompt
    asks the human for a prompt

--- game.js
+++ game.js
@@ -1,8 +1,29 @@
 function getComputerChoice () {
     const choices = ["rock", "paper", "scissors"];
     let randomChoice = choices[Math.floor(Math.random() * 3)];
+    console.log(randomChoice);
     return randomChoice;
 }
 
-console.log(getComputerChoice());
+getComputerChoice();
+
+
+function getHumanChoice () {
+    let humanChoice = prompt("rock, paper or scissors?\nChoice: ").toLowerCase();
+    if (humanChoice === "rock") {
+        console.log("You played rock.");
+
+    } else if (humanChoice === "paper") {
+        console.log("You played paper.");
+
+    } else if (humanChoice === "scisssors") {
+        console.log("You played scissors.");
+
+    }else {
+        console.log("Error. You didn't pick between the three choices.");
+    }
+    return humanChoice;
+}
+getHumanChoice();
+
 console.log("test");
